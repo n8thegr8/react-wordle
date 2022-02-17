0fafae4ab2a87bb93bc877697c36835cc5214cb2
n8thegr8 <nate@natemaxfield.com>
2022-02-16 21:17:24 -0500

    Wordle Updates
    Updated fonts and shadows
    Updated the top padding.

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,8 +2,8 @@ export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters, Dummy!'
+export const WORD_NOT_FOUND_MESSAGE = "You Idiot!  That's not a Word"
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
